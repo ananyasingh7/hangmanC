c8de7329571056044fa5768875ec3dda85058d8b
Ananya Singh <as2863@njit.edu>
2019-01-05 21:45:20 -0500

    working on layout

--- hangman.c
+++ hangman.c
@@ -2,6 +2,12 @@
 
 
 int main(){
-    printf("test\n");
+    printf("Welcome to Hangman!\n");
+    printf("Try to guess the secret word one letter at a time.\n");
+
+    printf("Player 1, please think of a word and then type it in: \n");
+    char word[256];
+    scanf("%s", word);
+    printf("%s\n", word); //use for debugging purposes 
     return 0;
 }
