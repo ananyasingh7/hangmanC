c5461163756dfdecd890758d8c01d5b097b16d43
Ananya Singh <as2863@njit.edu>
2019-01-05 23:56:30 -0500

    basic layout finished

--- hangman.c
+++ hangman.c
@@ -3,12 +3,30 @@
 #include<string.h> 
 #include<stdlib.h>
 
+void printArray(char * array, int length){
+    for(int i = 0; i<length; ++i){
+        printf("%c",array[i]);
+    }
+}
+
+void findLetter(char * word,char * tempWord, char c, int length){
+    for(int i = 0;i<length;++i){
+        if(c == word[i]){
+            tempWord[i]=c;
+        }
+    }
+    printArray(tempWord,length);
+}
+
+
+
+
 
 int main(){
     printf("Welcome to Hangman!\n");
     printf("Try to guess the secret word one letter at a time.\n");
 
-    printf("Player 1, please think of a word and then type it in: \n");
+    printf("Player 1, please think of a word in LOWERCASE and then type it in: \n");
     char word[256];
     char tempWord[256];
     scanf("%s", word);
@@ -17,19 +35,16 @@ int main(){
         tempWord[i]='*';
         tempWord[length-1]='\n';
     }
-
-    for(int i = 0; i<length; ++i){
-        printf("%c",tempWord[i]);
-    }
-
-
+ 
     printf("Player 2, you are up! You have 6 tries to guess a letter at a time. \n");
+    printArray(tempWord,length);
     bool finGame = false;
     int guesses = 6;
     while(finGame != true){
         char letter;
         printf("Enter a letter: \n");
         scanf(" %c",&letter);
+        findLetter(word, tempWord, letter, length);
         finGame = true;
     }
 
